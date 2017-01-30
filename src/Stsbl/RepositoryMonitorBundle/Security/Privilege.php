49ffc17aca9a98f6edbf272e989fb3254c2c8b14
Felix Jacobi <felix.jacobi@stsbl.de>
2017-01-30 10:54:59 +0100

    Made privilege container final.

--- src/Stsbl/RepositoryMonitorBundle/Security/Privilege.php
+++ src/Stsbl/RepositoryMonitorBundle/Security/Privilege.php
@@ -8,7 +8,7 @@ namespace Stsbl\RepositoryMonitorBundle\Security;
  * @author Felix Jacobi <felix.jacobi@stsbl.de>
  * @license GNU General Public License <http://gnu.org/licenses/gpl-3.0>
  */
-class Privilege 
+final class Privilege 
 {
     /**
      * Viewing status warnings for the IServ Portalserver
