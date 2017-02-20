a916d5514033b0eb1ee004cbb2df56f0abb69ad5
Felix Jacobi <felix.jacobi@stsbl.de>
2017-02-21 00:25:58 +0100

    Fixed undefined variable.

--- src/Stsbl/RepositoryMonitorBundle/EventListener/AdminDashboardListener.php
+++ src/Stsbl/RepositoryMonitorBundle/EventListener/AdminDashboardListener.php
@@ -31,7 +31,7 @@ class AdminDashboardListener implements AdminDashboardListenerInterface
         if ($event instanceof AdminHomeEvent && 'testing' === $mode) {
             // don't add message on testing updates, as it is shown as "dangerous", IDeskListener handles this case.
             return;
-        } else if ('unstable' === $mode || $mode === $testing) {
+        } else if ('unstable' === $mode || $mode === 'testing') {
             // Inject into admin dashboard
             $event->addContent(
                 'admin.stsblupdatemode',
